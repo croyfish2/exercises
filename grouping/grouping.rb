c76012c960eceba462638503791cd44a22769263
croyfish2 <68444931+croyfish2@users.noreply.github.com>
2020-07-20 12:00:51 -0400

    bump warning to correct ruby dot version

--- grouping/grouping.rb
+++ grouping/grouping.rb
@@ -1,6 +1,6 @@
 require_relative "lib/user_data_grouper"
 
-abort "ERROR: You are running the Grouping application on an unsupported version of Ruby (Ruby #{RUBY_VERSION} #{RUBY_RELEASE_DATE})! Please upgrade to at least Ruby v2.6.0" if RUBY_VERSION < "2.6"
+abort "ERROR: You are running the Grouping application on an unsupported version of Ruby (Ruby #{RUBY_VERSION} #{RUBY_RELEASE_DATE})! Please upgrade to at least Ruby v2.6.1" if RUBY_VERSION < "2.6.1"
 
 args = ARGV
 
